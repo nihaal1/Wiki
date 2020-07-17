2b9413b9e6f3b59d086d0f635edf713095b0feb8
nihaal1 <63922806+nihaal1@users.noreply.github.com>
2020-07-17 17:08:18 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 
-from . import util
+#tasks = [] This is a global list, so anyone from any session can access it
 
-def home(request):
-    return render(request, "encyclopedia/home.html")
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    #priority = forms.IntegerField(label= "Priority", min_value=1, max_value=5)
 
+# Create your views here.
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-        
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
+    return render(request, "tasks/index.html", {
+       # "tasks" : tasks ##The variable tasks can no longer be used as it is commented out
+       "tasks" : request.session["tasks"]
     })
+ 
+def add(request):
+    if request.method =="POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            #tasks.append(task)
+            request.session["tasks"] += [task]
+            return HttpResponseRedirect(reverse("tasks:index")) ##DO NOT put space b/w "tasks" and :
+        else:
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
 
-def get(request,name):
-    return render(request,f"{name}.html" )
+    return render(request,"tasks/add.html", {
+        "form" : NewTaskForm()
+    })
+    
